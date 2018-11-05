6ce30ada13c2140ac727615c4519f9a515865b70
Kimberly Becerril Fuentes <kymy@ciencias.unam.mx>
2018-11-05 16:12:44 -0600

    Add app.routing and login module folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRouting } from './app.routing';
+import { LoginComponent } from './modules/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.view.html',
+  styleUrls: ['./login.style.css']
+})
+export class LoginComponent {
+  title = 'login';
+}
