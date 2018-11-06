9252d1cd4eaf3ee3c810fe9234edff308afad0bb
Kimberly Becerril Fuentes <kymy@ciencias.unam.mx>
2018-11-05 23:07:41 -0600

    Add header content styles

--- src/app/modules/main/side-bar/books/books.component.ts
+++ src/app/modules/main/side-bar/books/books.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'books',
   templateUrl: './books.view.html',
-  styleUrls: ['./books.style.css']
+  styleUrls: ['./books.style.css', '../side-bar.style.css']
 })
 export class BooksComponent {
   title = 'books';
--- src/app/modules/main/side-bar/users/users.component.ts
+++ src/app/modules/main/side-bar/users/users.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'users',
   templateUrl: './users.view.html',
-  styleUrls: ['./users.style.css']
+  styleUrls: ['./users.style.css', '../side-bar.style.css']
 })
 export class UsersComponent {
   title = 'users';
